4463f50da6149272ec8a616e0db0fc819c7ebb8f
Jose Bigio <sirownsalot@gmail.com>
2017-02-25 20:35:34 -0600

    added cros support

--- server.js
+++ server.js
@@ -8,11 +8,14 @@ var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var request = require('request');
+var cors = require('cors')
+
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 var port = process.env.PORT || 8080;        // set our port
 
